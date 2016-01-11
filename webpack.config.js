46e2d74555c4b83fa66078286ed56b4d518aa2da
Sean Jacke <sean.jacke@learnship.com>
2016-01-11 16:57:09 +0100

    transpiles react and es6 code with babel loader for webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    exclude: /node_modules/,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: [ 'es2015' ]
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: [ 'es2015', 'react' ]
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            minify: { },
+            title: 'YouBetter',
+            bodyContent: '',
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ]
+};
